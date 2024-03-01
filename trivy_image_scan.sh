2820787ca7b6b2709e24f7e0a2d5b28534ad1c2d
vinay0094k <114790750+vinay0094k@users.noreply.github.com>
2024-03-01 12:46:09 +0530

    Update trivy_image_scan.sh

--- trivy_image_scan.sh
+++ trivy_image_scan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 trivyVersion="0.48.3"
-imageName=$1
-echo $imageName
+customImageName=$1
+echo $customImageName
 
 #-e TRIVY_GITHUB_TOKEN=$token: Sets an environment variable TRIVY_GITHUB_TOKEN with the provided token value. This is used to access private GitHub repositories during vulnerability scanning.
 #docker run --rm -v $WORKSPACE:/root/.cache/ -e TRIVY_GITHUB_TOKEN='token_github' aquasec/trivy:0.48.3 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:$trivyVersion -q image --exit-code 1 --severity CRITICAL --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:$trivyVersion -q image --exit-code 1 --severity CRITICAL --light $customImageName
 
 exit_code=$?
 echo "Exit Code : $exit_code"
